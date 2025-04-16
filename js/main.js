8629348fcadf37ca16e856493b40805eedcd9749
hirundorusticaii <63764749+hirundorusticaii@users.noreply.github.com>
2025-04-16 23:23:21 +0900

    追加：追従ボタン（画面上部へ移動）追加、おおざっぱなSPデザイン追加

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("OKEN");
+document.addEventListener("DOMContentLoaded", function () {
+    const pagetopButton = document.querySelector(".arrow-button");
+
+    window.addEventListener("scroll", function () {
+        const scrollTop =
+        window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollTop > 100) {
+            pagetopButton.style.opacity = "1";
+            pagetopButton.style.pointerEvents = "auto"; 
+        } else {
+            pagetopButton.style.opacity = "0";
+            pagetopButton.style.pointerEvents = "none"; 
+        }
+    });
+
+    pagetopButton.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
